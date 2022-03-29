2e6f717035969363e3772220509608de81c06a50
Ramiza1998 <ramizayakubu994@gmail.com>
2022-03-29 07:57:23 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import withLayout from './withLayout'
+
+function Home() {
+  return (
+    <div> 
+    <h1>Welcome To My HomePage</h1>
+    <div>
+    <p>In order to investigate the recruitment of a young European women by ISIS, journalist Amy Whittaker creates 
+  a new Facebook profile under the alias of Melody Nelson. She creates a persona online of a woman who has recently 
+  converted to Islam. Soon Amy is contacted by Bilel, an ISIS fighter from Syria. They begin to talk to each other regularly
+   and after some time she begins to develop real romantic feelings for him.
+  </p> 
+  <p>In order to investigate the recruitment of a young European women by ISIS, journalist Amy Whittaker creates 
+  a new Facebook profile under the alias of Melody Nelson. She creates a persona online of a woman who has recently 
+  converted to Islam. Soon Amy is contacted by Bilel, an ISIS fighter from Syria. They begin to talk to each other regularly
+   and after some time she begins to develop real romantic feelings for him.
+  </p> 
+
+  <p>In order to investigate the recruitment of a young European women by ISIS, journalist Amy Whittaker creates 
+  a new Facebook profile under the alias of Melody Nelson. She creates a persona online of a woman who has recently 
+  converted to Islam. Soon Amy is contacted by Bilel, an ISIS fighter from Syria. They begin to talk to each other regularly
+   and after some time she begins to develop real romantic feelings for him.
+  </p> 
+
+    </div>
+    </div>
+
+  
+
+  )
+}
+
+export default withLayout(Home)
